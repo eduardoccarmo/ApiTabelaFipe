eb9d96ac5a293d9632dd4c018c6d34f8c31fa1ef
eduardoccarmo <eduardo.ccarmo@outlook.com>
2023-08-01 21:17:04 -0300

    Criando estrutura inicial do projeto.

---
+++ ApiTabelaFipe.Infra/Context/ApiContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ApiTabelaFipe.Infra.Context
+{
+    public class ApiContext : DbContext
+    {
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+    }
+}
