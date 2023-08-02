23676c85bcfd4bd2254836ce789e46cad7a2702b
eduardoccarmo <eduardo.ccarmo@outlook.com>
2023-08-02 10:40:47 -0300

    Criando classes do projeto.

---
+++ ApiTabelaFipe.Domain/Models/Marca.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiTabelaFipe.Domain.Models
+{
+    public class Marca
+    {
+        public int Codigo { get; set; }
+
+        public string? Nome { get; set; }
+    }
+}
